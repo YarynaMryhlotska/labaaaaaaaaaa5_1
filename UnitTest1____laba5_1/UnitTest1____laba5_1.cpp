846bf2d2bd5abca81096b0ebf36c0d25401621fe
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-10-20 02:28:59 +0300

    1

---
+++ UnitTest1____laba5_1/UnitTest1____laba5_1.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../labaaaaaaaaa5(1)/labaaaaaa5(1).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1laba51
+{
+	TEST_CLASS(UnitTest1laba51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double c;
+			c = (h(s * s, t * t) + (h(s + t, 1) * h(s + t, 1))) / (1 + h(s * t, 2) * h(s * t, 2));
+			Assert::AreEqual(c, 0, 4);
+		}
+	};
+}
---
+++ labaaaaaaaaa5(1)/labaaaaaa5(1).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+double s, t;
+double h(const double x, const double y);
+int main()
+{
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (h(s * s, t * t) + (h(s + t, 1) * h(s + t, 1))) / (1 + h(s * t, 2) * h(s * t, 2));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double x, const double y)
+{
+	return (x * y / (1. + x * x * y * y));
+}
